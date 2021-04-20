1a89fc3b69c41ffaacd643ead402c7aa1229b70a
Sub-Dev <bilomarin@gmail.com>
2021-04-20 12:57:13 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -113,7 +113,51 @@ public function loadById($id){
 
 
 }
+public static function search($login){
+    $sql = new Sql();
+    return $sql->select ("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+':SEARCH'=>"%".$login."%"
 
+    ));
+
+}
+public function login($login,$password){
+
+    $sql = new Sql();
+
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE DESLOGIN = :LOGIN AND dessenha = :PASSWORD",array(
+
+        ":LOGIN"=>$login,
+        ":PASSWORD"=>$password
+
+
+    ));
+    if(count($results) > 0){
+
+        $row = $results[0];
+
+        $this->setIdusuario($row['idusuario']);
+        $this->setDeslogin($row['deslogin']);
+        $this->setDessenha($row['dessenha']);
+        $this->setDtcadastro(new DateTime ($row['dtcadastro']));
+
+
+    }
+    else{
+
+throw new Exception("Login e/ou senha invalidos.");
+
+    }
+
+
+}
+public static function getList(){
+
+
+    $sql = new Sql();
+    return $sql->select ("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+}
 
 public function __toString(){
 
@@ -122,7 +166,6 @@ return json_encode(array(
     "deslogin"=>$this->getDeslogin(),
     "dessenha"=>$this->getDessenha(),
     "dtcadastro"=>$this->getDtcadastro()->format("d/m/y H:i:s")
-
 
 ));
 
--- index.php
+++ index.php
@@ -10,8 +10,22 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-$root = new Usuario();
 
-$root->loadById(3);
+//Carrega um Usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carregando usuario pelo login e senha
+
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+echo $usuario;
